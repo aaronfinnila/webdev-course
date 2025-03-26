f24fb35beb46106d672a23e672e9728120cca146
Aaron <aafinn@utu.fi>
2025-03-26 22:08:13 +0200

    3.1-3.5

---
+++ part3/3.2/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const getTime = () => {
+  const time = new Date()
+  const timeOptions = {
+    weekday: 'short',
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }
+  const timeString = time.toLocaleString('en-EU', timeOptions)
+  return timeString
+}
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people<p>
+    <p>${getTime()} GMT+0200 (Eastern European Standard Time)<p>`
+  )
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
